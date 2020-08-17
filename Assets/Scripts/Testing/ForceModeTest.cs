5922f09f097906ecc4f998f6c5e13905443b833b
Max Linke <maxlinke@users.noreply.github.com>
2020-08-17 18:14:16 +0200

    tested forcemode
    > the "continuous" thing that it says in the descriptions is more of a "you have to apply this continuously" rather than "call the procedure once and we'll apply it continuously"...

---
+++ Assets/Scripts/Testing/ForceModeTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Testing {
+
+    public class ForceModeTest : MonoBehaviour {
+
+        [System.Serializable]
+        public struct Thing {
+            public Rigidbody rb;
+            public ForceMode fm;
+        }
+
+        [SerializeField] Thing[] things;
+        [SerializeField] Vector3 vector;
+
+        void Start () {
+            foreach(var thing in things){
+                thing.rb.AddForce(vector, thing.fm);
+            }   
+        }
+        
+    }
+
+}
