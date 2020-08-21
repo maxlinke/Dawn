2ec4a88ca4c65cf7ceb95e01a7229fdb40e4a610
Max Linke <maxlinke@users.noreply.github.com>
2020-08-21 17:27:15 +0200

    tested physic materials a bit more
    > negative friction is not allowed. would be funny tho.
    > dynamic friction is, as the name suggests, how much an object gets slowed down when sliding
    > no idea about static friction tho. doesn't seem to make a difference...

--- Assets/Scripts/Testing/PMTest.cs
+++ Assets/Scripts/Testing/PMTest.cs
@@ -4,20 +4,50 @@ namespace Testing {
 
     public class PMTest : MonoBehaviour {
 
+        [SerializeField] KeyCode addForceKey = default;
         [SerializeField] Rigidbody[] rbs = default;
         [SerializeField] Vector3 rbForce = default;
         [SerializeField] Collider scriptedDefaultPMTarget = default;
+        [SerializeField] Collider customPMTarget = default;
+        [SerializeField] float customStaticFriction = 0.6f;
+        [SerializeField] float customDynamicFriction = 0.6f;
+        [SerializeField] float customBounciness = 0f;
+        [SerializeField] PhysicMaterialCombine customFrictionCombine = PhysicMaterialCombine.Average;
+        [SerializeField] PhysicMaterialCombine customBounceCombine = PhysicMaterialCombine.Average;
 
         void Start () {
             var defaultPM = new PhysicMaterial();
-            var log = $"{nameof(PhysicMaterial)}";
-            log += $"\nbounceCombine: {defaultPM.bounceCombine}";
-            log += $"\nbounciness: {defaultPM.bounciness}";
-            log += $"\ndynamicFriction: {defaultPM.dynamicFriction}";
-            log += $"\nfrictionCombine: {defaultPM.frictionCombine}";
-            log += $"\nstaticFriction: {defaultPM.staticFriction}";
-            Debug.Log(log);
+            Debug.Log($"default: \n{GetPMLog(defaultPM)}");
             scriptedDefaultPMTarget.sharedMaterial = defaultPM;
+            var customPM = new PhysicMaterial();
+            customPM.staticFriction = customStaticFriction;
+            customPM.dynamicFriction = customDynamicFriction;
+            customPM.bounciness = customBounciness;
+            customPM.frictionCombine = customFrictionCombine;
+            customPM.bounceCombine = customBounceCombine;
+            Debug.Log($"custom: \n{GetPMLog(customPM)}");
+            customPMTarget.sharedMaterial = customPM;
+            PushRBs();
+            
+            string GetPMLog (PhysicMaterial inputPM) {
+                var outputLog = string.Empty;
+                outputLog += $"\nbounceCombine: {inputPM.bounceCombine}";
+                outputLog += $"\nbounciness: {inputPM.bounciness}";
+                outputLog += $"\ndynamicFriction: {inputPM.dynamicFriction}";
+                outputLog += $"\nfrictionCombine: {inputPM.frictionCombine}";
+                outputLog += $"\nstaticFriction: {inputPM.staticFriction}";
+                return outputLog;
+            }
+        }
+
+
+        void Update () {
+            if(Input.GetKeyDown(addForceKey)){
+                PushRBs();
+            }
+        }
+
+        void PushRBs () {
             foreach(var rb in rbs){
                 rb.AddForce(rbForce);
             }
