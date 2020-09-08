dbbe562da11aa181d1358d7c1cffce10b99210c1
Max Linke <maxlinke@users.noreply.github.com>
2020-09-08 22:23:35 +0200

    uv- and vertex-offset water
    > didn't port but reimplemted my old "oldschool water"
    > also made a customeditor for it because easy
    > customeditor was easy because i made the defaultcustomlmeditor easily extendable

---
+++ Assets/Shaders/Editor/CustomWaterEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ShaderEditors {
+
+    public class CustomWaterEditor : DefaultCustomLMEditor {
+
+        public override void OnGUI (MaterialEditor editor, MaterialProperty[] properties) {
+            var targetMat = GetTargetMaterial(editor);
+            DrawDiffuseProperties(editor, properties);
+            DrawEmissionProperties(editor, properties, targetMat);
+            DrawFlowProperties(editor, properties, targetMat);
+            DrawFooterProperties(editor, properties);
+        }
+
+        void DrawFlowProperties (MaterialEditor editor, MaterialProperty[] properties, Material targetMat) {
+            GUILayout.Label("Flow", EditorStyles.boldLabel);
+            DrawIndented(() => {
+                var flowTex = FindProperty("_FlowTex", properties);
+                var flowSpeed = FindProperty("_FlowSpeed", properties);
+                var flowDistortion = FindProperty("_FlowDistortion", properties);
+                editor.TexturePropertySingleLine(new GUIContent(flowTex.displayName), flowTex);
+                editor.TextureScaleOffsetProperty(flowTex);
+                editor.ShaderProperty(flowSpeed, flowSpeed.displayName);
+                editor.ShaderProperty(flowDistortion, flowDistortion.displayName);
+                var offsetToggle = FindProperty("_OffsetVertices", properties);
+                editor.ShaderProperty(offsetToggle, offsetToggle.displayName);
+                if(targetMat.IsKeywordEnabled("_OFFSET_VERTICES")){
+                    var offsetDir = FindProperty("_VertexOffsetDir", properties);
+                    editor.ShaderProperty(offsetDir, offsetDir.displayName);
+                }
+            });
+        }
+        
+    }
+
+}
--- Assets/Shaders/Editor/DefaultCustomLMEditor.cs
+++ Assets/Shaders/Editor/DefaultCustomLMEditor.cs
@@ -6,8 +6,25 @@ namespace ShaderEditors {
     public class DefaultCustomLMEditor : CustomShaderGUI {
 
         public override void OnGUI (MaterialEditor editor, MaterialProperty[] properties) {
-            var targetMat = editor.target as Material;
+            var targetMat = GetTargetMaterial(editor);
+            DrawDiffuseProperties(editor, properties);
+            if(targetMat.HasProperty("_SpecColor")){
+                DrawSpecularProperties(editor, properties, targetMat);
+            }
+            if(targetMat.HasProperty("_BumpMap")){
+                DrawNormalMapProperties(editor, properties);
+            }
+            if(targetMat.HasProperty("_Emissive")){
+                DrawEmissionProperties(editor, properties, targetMat);
+            }
+            DrawFooterProperties(editor, properties);
+        }
 
+        protected Material GetTargetMaterial (MaterialEditor editor) {
+            return editor.target as Material;
+        }
+
+        protected void DrawDiffuseProperties (MaterialEditor editor, MaterialProperty[] properties) {
             GUILayout.Label("Diffuse", EditorStyles.boldLabel);
             DrawIndented(() => {
                 var mainColProp = FindProperty("_Color", properties);
@@ -15,59 +32,63 @@ namespace ShaderEditors {
                 editor.TexturePropertySingleLine(new GUIContent(mainTexProp.displayName), mainTexProp, mainColProp);
                 DrawIndented(() => {editor.TextureScaleOffsetProperty(mainTexProp);});
             });
+        }
 
-            if(targetMat.HasProperty("_SpecColor")){
-                GUILayout.Label("Specular", EditorStyles.boldLabel);
-                DrawIndented(() => {
-                    var specHardProp = FindProperty("_SpecHardness", properties);
-                    editor.ShaderProperty(specHardProp, specHardProp.displayName);
-                    var sampleSpecProp = FindProperty("_SampleSpecularMap", properties);
-                    editor.ShaderProperty(sampleSpecProp, sampleSpecProp.displayName);
-                    var specColProp = FindProperty("_SpecColor", properties);
-                    if(targetMat.IsKeywordEnabled("_SPECULARMAP")){
-                        var specTexProp = FindProperty("_SpecTex", properties);
-                        editor.TexturePropertySingleLine(new GUIContent(specTexProp.displayName), specTexProp, specColProp);
-                        DrawIndented(() => {editor.TextureScaleOffsetProperty(specTexProp);});
-                    }else{
-                        editor.ShaderProperty(specColProp, specColProp.displayName);
-                    }
-                });
-            }
+        protected void DrawSpecularProperties (MaterialEditor editor, MaterialProperty[] properties, Material targetMat) {
+            GUILayout.Label("Specular", EditorStyles.boldLabel);
+            DrawIndented(() => {
+                var specHardProp = FindProperty("_SpecHardness", properties);
+                editor.ShaderProperty(specHardProp, specHardProp.displayName);
+                var sampleSpecProp = FindProperty("_SampleSpecularMap", properties);
+                editor.ShaderProperty(sampleSpecProp, sampleSpecProp.displayName);
+                var specColProp = FindProperty("_SpecColor", properties);
+                if(targetMat.IsKeywordEnabled("_SPECULARMAP")){
+                    var specTexProp = FindProperty("_SpecTex", properties);
+                    editor.TexturePropertySingleLine(new GUIContent(specTexProp.displayName), specTexProp, specColProp);
+                    DrawIndented(() => {editor.TextureScaleOffsetProperty(specTexProp);});
+                }else{
+                    editor.ShaderProperty(specColProp, specColProp.displayName);
+                }
+            });
+        }
 
-            if(targetMat.HasProperty("_BumpMap")){
-                GUILayout.Label("Normals", EditorStyles.boldLabel);
-                DrawIndented(() => {
-                    var normalMapProp = FindProperty("_BumpMap", properties);
-                    editor.TexturePropertySingleLine(new GUIContent(normalMapProp.displayName), normalMapProp);
-                    DrawIndented(() => {editor.TextureScaleOffsetProperty(normalMapProp);});
-                });
-            }
+        protected void DrawNormalMapProperties (MaterialEditor editor, MaterialProperty[] properties) {
+            GUILayout.Label("Normals", EditorStyles.boldLabel);
+            DrawIndented(() => {
+                var normalMapProp = FindProperty("_BumpMap", properties);
+                editor.TexturePropertySingleLine(new GUIContent(normalMapProp.displayName), normalMapProp);
+                DrawIndented(() => {editor.TextureScaleOffsetProperty(normalMapProp);});
+            });
+        }
 
-            if(targetMat.HasProperty("_Emissive")){
-                GUILayout.Label("Emission", EditorStyles.boldLabel);
-                DrawIndented(() => {
-                    var emissiveProp = FindProperty("_Emissive", properties);
-                    EditorGUI.BeginChangeCheck();
-                    editor.ShaderProperty(emissiveProp, emissiveProp.displayName);
-                    if(EditorGUI.EndChangeCheck()){
-                        editor.RegisterPropertyChangeUndo("Global Illumination Flags");
-                        if(targetMat.IsKeywordEnabled("_EMISSIVE")){
-                            targetMat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
-                        }else{
-                            targetMat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
-                        }
-                    }
+        protected void DrawEmissionProperties (MaterialEditor editor, MaterialProperty[] properties, Material targetMat) {
+            GUILayout.Label("Emission", EditorStyles.boldLabel);
+            DrawIndented(() => {
+                var emissiveProp = FindProperty("_Emissive", properties);
+                EditorGUI.BeginChangeCheck();
+                editor.ShaderProperty(emissiveProp, emissiveProp.displayName);
+                if(EditorGUI.EndChangeCheck()){
+                    editor.RegisterPropertyChangeUndo("Global Illumination Flags");
                     if(targetMat.IsKeywordEnabled("_EMISSIVE")){
-                        var emitColProp = FindProperty("_EmissionColor", properties);
-                        var emitTexProp = FindProperty("_EmissionTex", properties);
-                        editor.TexturePropertySingleLine(new GUIContent(emitTexProp.displayName), emitTexProp, emitColProp);
-                        DrawIndented(() => {editor.TextureScaleOffsetProperty(emitTexProp);});
-                        DrawFullGISelection(editor, targetMat);
+                        targetMat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+                    }else{
+                        targetMat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
                     }
-                });
-            }
+                }
+                if(targetMat.IsKeywordEnabled("_EMISSIVE")){
+                    var emitColProp = FindProperty("_EmissionColor", properties);
+                    var emitTexProp = FindProperty("_EmissionTex", properties);
+                    editor.TexturePropertySingleLine(new GUIContent(emitTexProp.displayName), emitTexProp, emitColProp);
+                    DrawIndented(() => {editor.TextureScaleOffsetProperty(emitTexProp);});
+                    DrawFullGISelection(editor, targetMat);
+                }
+            });
+        }
 
-            GUILayout.Space(10f);
+        protected void DrawFooterProperties (MaterialEditor editor, MaterialProperty[] properties, bool addSpace = true) {
+            if(addSpace){
+                GUILayout.Space(10f);
+            }
             DrawRenderingOptionsIfExists(editor, properties);
             DrawDefaultBottomProperties(editor);
         }
