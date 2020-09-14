637f1655e2bc5afbd3b0006d85d7fc7be6ceaf96
Max Linke <maxlinke@users.noreply.github.com>
2020-09-14 22:27:06 +0200

    quick update to enumflags drawer
    > now properly supports multi-object editing

--- Assets/Scripts/Editor Tools/EnumFlagsAttribute.cs
+++ Assets/Scripts/Editor Tools/EnumFlagsAttribute.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿// source: https://answers.unity.com/questions/486694/default-editor-enum-as-flags-.html
+
+using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -19,7 +21,13 @@ public class EnumFlagsAttributeDrawer : PropertyDrawer {
     }
 
     public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
-        property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumDisplayNames);
+        EditorGUI.showMixedValue = property.hasMultipleDifferentValues;
+        EditorGUI.BeginChangeCheck();
+        var origValue = property.intValue;
+        var newValue = EditorGUI.MaskField(position, label, origValue, property.enumDisplayNames);
+        if(EditorGUI.EndChangeCheck()){
+            property.intValue = newValue;
+        }
     }
 
 }
