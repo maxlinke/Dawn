45d756d18a39630a931d32649298d86407ed32aa
Max Linke <maxlinke@users.noreply.github.com>
2020-09-17 02:40:23 +0200

    some cleanup
    > removed terminal velocity stuff from test
    > deleted the "buoyancy test" that was the template for the drag test thingy that i didn't do anything with

--- Assets/Scripts/Testing/Test.cs
+++ Assets/Scripts/Testing/Test.cs
@@ -4,29 +4,12 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-    [SerializeField] float drag = 1f;
-    [SerializeField] float gravity = 9.81f;
-    [SerializeField] float deltaTime = 0.02f;
-
     void Awake () {
         
     }
 
     void Update () {
         
-    }
-
-    [ContextMenu("Log Terminal Velocity")]
-    void LogTerminalVelocity () {
-        Debug.Log(GetTerminalVelocity());
-    }
-
-    float GetTerminalVelocity () {
-        if(drag == 0f){
-            return float.PositiveInfinity;
-        }
-        var x = 1f - deltaTime * drag;
-        return (gravity * x) / drag;
     }
 	
 }
