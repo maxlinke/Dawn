605fe27707def1d516495bff70b3d865b3a78a53
Max Linke <maxlinke@users.noreply.github.com>
2020-09-30 04:03:53 +0200

    added iso tri option to plane/terrain generator

--- Assets/Scripts/Geometry Generators/Editor/PlaneGeneratorEditor.cs
+++ Assets/Scripts/Geometry Generators/Editor/PlaneGeneratorEditor.cs
@@ -5,10 +5,12 @@ namespace GeometryGenerators {
     [CustomEditor(typeof(PlaneGenerator))]
     public class PlaneGeneratorEditor : GeometryGeneratorEditor { 
 
+        SerializedProperty tileModeProp;
         SerializedProperty triModeProp;
 
         protected override void OnEnable () {
             base.OnEnable();
+            tileModeProp = serializedObject.FindProperty("tileMode");
             triModeProp = serializedObject.FindProperty("triMode");
         }
 
@@ -16,13 +18,20 @@ namespace GeometryGenerators {
             if(base.DrawPropertyCustom(property)){
                 return true;
             }
-            if(property.name.Equals("triSeed")){
-                if(triModeProp.enumValueIndex == (int)(PlaneGenerator.TriMode.Random)){
-                    EditorTools.DrawIndented(() => EditorGUILayout.PropertyField(property, true));
-                }
-                return true;
+            switch(property.name){
+                case "triMode":
+                    if(tileModeProp.enumValueIndex == (int)(PlaneGenerator.TileMode.Quads)){
+                        EditorGUILayout.PropertyField(property, true);
+                    }
+                    return true;
+                case "triSeed":
+                    if(tileModeProp.enumValueIndex == (int)(PlaneGenerator.TileMode.Quads) && triModeProp.enumValueIndex == (int)(PlaneGenerator.TriMode.Random)){
+                        EditorTools.DrawIndented(() => EditorGUILayout.PropertyField(property, true));
+                    }
+                    return true;
+                default:
+                    return false;
             }
-            return false;
         }
 
     }
--- Assets/Scripts/Geometry Generators/PlaneGenerator.cs
+++ Assets/Scripts/Geometry Generators/PlaneGenerator.cs
@@ -17,12 +17,18 @@ namespace GeometryGenerators {
             LongestDiagonal
         }
 
+        public enum TileMode {
+            Quads,
+            IsoTris
+        }
+
         [Header("Plane Settings")]
+        [SerializeField] protected TileMode tileMode = TileMode.Quads;
         [SerializeField] [Range(1, 254)] protected int xTiles = 32;
         [SerializeField] [Range(1, 254)] protected int zTiles = 32;
         [SerializeField] protected Vector2 tileSize = Vector2.one;
         [SerializeField] protected TriMode triMode = TriMode.Fixed;
-        [SerializeField] string triSeed = string.Empty;
+        [SerializeField] protected string triSeed = string.Empty;
 
         [Header("UV Settings")]
         [SerializeField] protected UVMode uvMode = UVMode.Normalized;
@@ -39,97 +45,31 @@ namespace GeometryGenerators {
         }
 
         protected override Mesh CreateMesh () {
-            int xVerts = xTiles + 1;
-            int zVerts = zTiles + 1;
-            int numberOfVerts = xVerts * zVerts;
-            Vector3[] vertices = new Vector3[numberOfVerts];
-            Vector2[] texcoords = new Vector2[numberOfVerts];
-
-            float iOffset = (float)xTiles / 2f;
-            float jOffset = (float)zTiles / 2f;
-            for(int j=0; j<zVerts; j++){
-                for(int i=0; i<xVerts; i++){
-                    int index = (j * xVerts) + i;
-                    var pos = GetVertexCoordinates(i, j, iOffset, jOffset);
-                    pos += GetAdditionalVertexOffset(pos);
-                    vertices[index] = pos;
-                    float fracI = (float)i/xTiles;
-                    float fracJ = (float)j/zTiles;
-                    switch(uvMode){
-                        case UVMode.Normalized:
-                            texcoords[index] = new Vector2(fracI, fracJ) * uvScale;
-                            break;
-                        case UVMode.VertexCoordinates:
-                            texcoords[i] = new Vector2(pos.x, pos.z) * uvScale;
-                            break;
-                        default:
-                            Debug.LogError($"Unknown {nameof(UVMode)} \"{uvMode}\"!");
-                            return null;
-                    }
-                }
-            }            
-
-            int numberOfTris = xTiles * zTiles * 2;
-            int[] triangles = new int[numberOfTris * 3];
-
-            int rngSeed;
-            if(triSeed == null || triSeed.Length == 0){
-                rngSeed = Random.value.GetHashCode();
-            }else{
-                rngSeed = triSeed.Trim().GetHashCode();
+            Mesh output = null;
+            Vector3[] vertices = null;
+            Vector2[] texcoords = null;
+            int[] triangles = null;
+            if(tileMode == TileMode.Quads && TryMakeQuadMesh(out vertices, out texcoords, out triangles)){
+                output = new Mesh();
             }
-            System.Random triRNG = new System.Random(rngSeed);
-            for(int j=0; j<zTiles; j++){
-                for(int i=0; i<xTiles; i++){
-                    int quad = (j * xTiles) + i;
-                    int triStart = quad * 6;
-                    int vertStart = (j * xVerts) + i;
-                    var bottomLeft = vertStart;
-                    var bottomRight = vertStart + 1;
-                    var topLeft = vertStart + xVerts;
-                    var topRight = vertStart + xVerts + 1;
-                    var sqrDiag1 = (vertices[bottomLeft] - vertices[topRight]).sqrMagnitude;
-                    var sqrDiag2 = (vertices[topLeft] - vertices[bottomRight]).sqrMagnitude;
-                    bool alternate;
-                    switch(triMode){
-                        case TriMode.Fixed:
-                            alternate = false;
-                            break;
-                        case TriMode.FixedAlternate:
-                            alternate = true;
-                            break;
-                        case TriMode.Random:
-                            alternate = triRNG.NextDouble() < 0.5;
-                            break;
-                        case TriMode.ShortestDiagonal:
-                            alternate = sqrDiag1 > sqrDiag2;
-                            break;
-                        case TriMode.LongestDiagonal:
-                            alternate = sqrDiag1 < sqrDiag2;
-                            break;
-                        default:
-                            Debug.LogError($"Unknown {nameof(TriMode)} \"{triMode}\"!");
-                            return null;
-                    }
-                    if(!alternate){
-                        triangles[triStart + 0] = bottomLeft;
-                        triangles[triStart + 1] = topLeft;
-                        triangles[triStart + 2] = topRight;
-                        triangles[triStart + 3] = bottomLeft;
-                        triangles[triStart + 4] = topRight;
-                        triangles[triStart + 5] = bottomRight;
-                    }else{
-                        triangles[triStart + 0] = bottomLeft;
-                        triangles[triStart + 1] = topLeft;
-                        triangles[triStart + 2] = bottomRight;
-                        triangles[triStart + 3] = topRight;
-                        triangles[triStart + 4] = bottomRight;
-                        triangles[triStart + 5] = topLeft;
-                    }
-                }
+            if(tileMode == TileMode.IsoTris && TryMakeIsoTriMesh(out vertices, out texcoords, out triangles)){
+                output = new Mesh();
             }
-
-            Mesh output = new Mesh();
+            if(output == null){
+                return null;
+            }
+            switch(uvMode){
+                case UVMode.Normalized:
+                    break;
+                case UVMode.VertexCoordinates:
+                    for(int i=0; i<texcoords.Length; i++){
+                        texcoords[i] = vertices[i].XZ() * uvScale;
+                    }
+                    break;
+                default:
+                    Debug.LogError($"Unknown {nameof(UVMode)} \"{uvMode}\"!");
+                    return null;
+            }
             output.name = "Custom Plane";
             output.vertices = vertices;
             output.uv = texcoords;
@@ -145,14 +85,29 @@ namespace GeometryGenerators {
         }
 
         protected override void DrawGizmos () {
-            var points = new Vector3[] { 
-                GetVertexCoordinates(0, 0),
-                GetVertexCoordinates(0, zTiles),
-                GetVertexCoordinates(xTiles, zTiles),
-                GetVertexCoordinates(xTiles, 0) 
-            };
-            for(int i=0; i<4; i++){
-                DrawLine(points[i], points[(i+1)%4]);
+            Vector3[] points;
+            if(tileMode == TileMode.Quads){
+                points = new Vector3[]{ 
+                    GetVertexCoordinates(0, 0),
+                    GetVertexCoordinates(0, zTiles),
+                    GetVertexCoordinates(xTiles, zTiles),
+                    GetVertexCoordinates(xTiles, 0) 
+                };
+            }else if(tileMode == TileMode.IsoTris){
+                var d = GetIsoDimensions();
+                var x = d.x / 2f;
+                var z = d.y / 2f;
+                points = new Vector3[]{
+                    new Vector3(-x, 0f, -z),
+                    new Vector3(-x, 0f,  z),
+                    new Vector3( x, 0f,  z),
+                    new Vector3( x, 0f, -z)
+                };
+            }else{
+                points = new Vector3[]{};
+            }
+            for(int i=0; i<points.Length; i++){
+                DrawLine(points[i], points[(i+1)%points.Length]);
             }
 
             void DrawLine (Vector3 a, Vector3 b) {
@@ -160,6 +115,185 @@ namespace GeometryGenerators {
                     transform.TransformPoint(a),
                     transform.TransformPoint(b)
                 );
+            }
+        }
+
+        private bool TryMakeQuadMesh (out Vector3[] vertices, out Vector2[] texcoords, out int[] triangles) {
+            int xVerts = xTiles + 1;
+            int zVerts = zTiles + 1;
+            int numberOfVerts = xVerts * zVerts;
+            vertices = new Vector3[numberOfVerts];
+            texcoords = new Vector2[numberOfVerts];
+            int numberOfTris = xTiles * zTiles * 2;
+            triangles = new int[numberOfTris * 3];
+            if(!TryMakeVertsAndTexcoords(vertices, texcoords)){
+                return false;
+            }
+            return TryMakeTriangles(vertices, triangles);
+
+            bool TryMakeVertsAndTexcoords (Vector3[] verts, Vector2[] uvs) {
+                float iOffset = (float)xTiles / 2f;
+                float jOffset = (float)zTiles / 2f;
+                for(int j=0; j<zVerts; j++){
+                    for(int i=0; i<xVerts; i++){
+                        int index = (j * xVerts) + i;
+                        var pos = GetVertexCoordinates(i, j, iOffset, jOffset);
+                        pos += GetAdditionalVertexOffset(pos);
+                        verts[index] = pos;
+                        float fracI = (float)i/xTiles;
+                        float fracJ = (float)j/zTiles;
+                        uvs[index] = new Vector2(fracI, fracJ) * uvScale;
+                    }
+                }
+                return true;
+            }
+
+            System.Random GetTriRNG () {
+                int rngSeed;
+                if(triSeed == null || triSeed.Length == 0){
+                    rngSeed = Random.value.GetHashCode();
+                }else{
+                    rngSeed = triSeed.Trim().GetHashCode();
+                }
+                return new System.Random(rngSeed);
+            }
+
+            System.Func<float, float, double, bool> GetTriangulationPatternFunction () {
+                switch(triMode){
+                    case TriMode.Fixed:
+                        return (dia1, dia2, rng) => false;
+                    case TriMode.FixedAlternate:
+                        return (dia1, dia2, rng) => true;
+                    case TriMode.Random:
+                        return (dia1, dia2, rng) => (rng < 0.5);
+                    case TriMode.ShortestDiagonal:
+                        return (dia1, dia2, rng) => (dia1 > dia2);
+                    case TriMode.LongestDiagonal:
+                        return (dia1, dia2, rng) => (dia1 < dia2);
+                    default:
+                        Debug.LogError($"Unknown {nameof(TriMode)} \"{triMode}\"!");
+                        return null;
+                }
+            }
+
+            bool TryMakeTriangles (Vector3[] verts, int[] tris) {
+                var triRNG = GetTriRNG();
+                var triPattern = GetTriangulationPatternFunction();
+                if(triPattern == null){
+                    return false;
+                }
+                for(int j=0; j<zTiles; j++){
+                    for(int i=0; i<xTiles; i++){
+                        int quad = (j * xTiles) + i;
+                        int triStart = quad * 6;
+                        int vertStart = (j * xVerts) + i;
+
+                        var bottomLeft = vertStart;
+                        var bottomRight = vertStart + 1;
+                        var topLeft = vertStart + xVerts;
+                        var topRight = vertStart + xVerts + 1;
+
+                        var sqrDiag1 = (verts[bottomLeft] - verts[topRight]).sqrMagnitude;
+                        var sqrDiag2 = (verts[topLeft] - verts[bottomRight]).sqrMagnitude;
+                        
+                        var alternate = triPattern(sqrDiag1, sqrDiag2, triRNG.NextDouble());
+                        if(!alternate){
+                            tris[triStart + 0] = bottomLeft;
+                            tris[triStart + 1] = topLeft;
+                            tris[triStart + 2] = topRight;
+                            tris[triStart + 3] = bottomLeft;
+                            tris[triStart + 4] = topRight;
+                            tris[triStart + 5] = bottomRight;
+                        }else{
+                            tris[triStart + 0] = bottomLeft;
+                            tris[triStart + 1] = topLeft;
+                            tris[triStart + 2] = bottomRight;
+                            tris[triStart + 3] = topRight;
+                            tris[triStart + 4] = bottomRight;
+                            tris[triStart + 5] = topLeft;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+        private Vector2 GetIsoDimensions () {
+            var width = tileSize.x * (0.5f * (xTiles + 1));
+            var height = tileSize.y * (zTiles * Mathf.Sqrt(3) * 0.5f);
+            return new Vector2(width, height);
+        }
+
+        private bool TryMakeIsoTriMesh (out Vector3[] vertices, out Vector2[] texcoords, out int[] triangles) {
+            var isoDims = GetIsoDimensions();
+            GetXCoordArrays(isoDims.x, out var xCoordsBase, out var xCoordsOffset);
+            var baseLineCount = 1 + (zTiles / 2);
+            var offsetLineCount = 1 + ((zTiles - 1) / 2);
+            vertices = new Vector3[(baseLineCount * xCoordsBase.Length) + (offsetLineCount * xCoordsOffset.Length)];
+            texcoords = new Vector2[vertices.Length];
+            triangles = new int[3 * (xTiles * zTiles)];
+            MakeVertAndTexCoords(isoDims.x, isoDims.y, vertices, texcoords);
+            MakeTris(vertices, triangles);
+            return true;
+
+            void GetXCoordArrays (float width, out float[] outputBaseCoords, out float[] outputOffsetCoords) {
+                var baseVertCount = ((xTiles + 1) / 2) + 1;
+                var offsetVertCount = baseVertCount - (xTiles % 2);
+                outputBaseCoords = new float[baseVertCount];
+                outputOffsetCoords = new float[offsetVertCount];
+                for(int i=0; i<(baseVertCount + offsetVertCount); i++){
+                    var fracI = (float)i / (baseVertCount + offsetVertCount - 1);
+                    var x = (fracI - 0.5f) * width;
+                    if(i%2 == 0){
+                        outputBaseCoords[i/2] = x;
+                    }else{
+                        outputOffsetCoords[i/2] = x;
+                    }
+                }
+            }
+
+            void MakeVertAndTexCoords (float width, float height, Vector3[] verts, Vector2[] uvs) {
+                int vertCounter = 0;
+                for(int z=0; z<=zTiles; z++){
+                    var fracZ = (float)z / zTiles;
+                    var zCoord = (fracZ - 0.5f) * height;
+                    var isBaseLine = ((z%2) == 0);
+                    var xCoords = isBaseLine ? xCoordsBase : xCoordsOffset;
+                    for(int x=0; x<xCoords.Length; x++){
+                        var pos = new Vector3(xCoords[x], 0f, zCoord);
+                        pos += GetAdditionalVertexOffset(pos);
+                        verts[vertCounter] = pos;
+                        uvs[vertCounter] = new Vector2((pos.x / width) + 0.5f, fracZ) * uvScale;
+                        vertCounter++;
+                    }
+                }
+            }
+
+            void MakeTris (Vector3[] verts, int[] tris) {
+                var equalLineLengths = (xCoordsBase.Length == xCoordsOffset.Length);
+                var processedLineVerts = 0;
+                for(int j=0; j<zTiles; j++){
+                    var isBaseLine = ((j % 2) == 0);
+                    var lineVertCount = (isBaseLine ? xCoordsBase.Length : xCoordsOffset.Length);
+                    var bottomVert = processedLineVerts;
+                    var topVert = bottomVert + lineVertCount;
+                    var pointy = !isBaseLine;
+                    for(int i=0; i<xTiles; i++){
+                        var index = j * xTiles + i;
+                        var triStart = 3 * index;
+                        tris[triStart+0] = bottomVert;
+                        tris[triStart+1] = topVert;
+                        if(pointy){
+                            topVert++;
+                            tris[triStart+2] = topVert;
+                        }else{
+                            bottomVert++;
+                            tris[triStart+2] = bottomVert;
+                        }
+                        pointy = !pointy;
+                    }
+                    processedLineVerts += lineVertCount;
+                }
             }
         }
 
