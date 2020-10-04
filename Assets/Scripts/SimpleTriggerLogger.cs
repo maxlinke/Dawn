da8f618892b0750c5a41ef05bd2ae40407672ca1
Max Linke <maxlinke@users.noreply.github.com>
2020-10-04 23:07:58 +0200

    started removing old and unnecessary scripts

--- Assets/Scripts/SimpleTriggerLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class SimpleTriggerLogger : MonoBehaviour {
-
-    void OnTriggerEnter (Collider otherCollider) {
-        Debug.Log($"Collider \"{otherCollider.name}\" entered trigger \"{this.name}\"");
-    }
-	
-}
