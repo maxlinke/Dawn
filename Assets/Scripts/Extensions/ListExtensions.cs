d81324a2c7f388c0ca73e6194aa753d3cd14c00a
Max Linke <maxlinke@users.noreply.github.com>
2020-10-05 16:39:23 +0200

    updated water from triggerstay to enter/exit
    > also added list extensions for removing null entries from a list

---
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public static class ListExtensions {
+
+    public static void RemoveNullEntries<T> (this List<T> list) where T : class {
+        for(int i=0; i<list.Count; i++){
+            if(list[i] == null){
+                list.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    public static string ContentsToString<T> (this List<T> list) {
+        string output = string.Empty;
+        for(int i=0; i<list.Count; i++){
+            T t = list[i];
+            if(t == null){
+                output += $"{i} null\n";
+            }else{
+                output += $"{i} : {t.ToString()}\n";
+            }
+        }
+        return output;
+    }
+	
+}
--- Assets/Scripts/Physics/Water/WaterBody.cs
+++ Assets/Scripts/Physics/Water/WaterBody.cs
@@ -3,16 +3,12 @@ using UnityEngine;
 
 public abstract class WaterBody : MonoBehaviour {
 
-    // public const float CONTAINS_DIST = 0.01f;
-    public const float CONTAINS_DIST = 0f;  // this seems to work much better but i'm afraid the preciseness will cause issues...
-    public const float CONTAINS_DIST_SQR = CONTAINS_DIST * CONTAINS_DIST;
-
     public const float MAX_RIGIDBODY_SIZE_ASSUMPTION = 100f;
 
     private static List<WaterBody> waterBodies = new List<WaterBody>();
 
     [Header("Water Properties")]
-    [SerializeField] WaterPhysics physics = default;
+    [SerializeField] WaterPhysics waterPhysics = default;
     [SerializeField] WaterFog fog = default;
 
     bool initialized = false;
@@ -20,11 +16,30 @@ public abstract class WaterBody : MonoBehaviour {
     Vector3 torqueNoise;
 
     public WaterFog Fog => fog;
-    public WaterPhysics WaterPhysics => physics;
+    public WaterPhysics WaterPhysics => waterPhysics;
 
     protected abstract bool CollidersNotNull { get; }
     protected abstract Collider MainCollider { get; }
     public abstract IEnumerator<Collider> GetEnumerator ();
+
+    public static bool ColliderContainsPoint (Collider col, Vector3 worldPoint) {
+        return (col.ClosestPoint(worldPoint) - worldPoint).sqrMagnitude <= Mathf.Epsilon;
+    }
+
+    public abstract bool ContainsPoint (Vector3 worldPoint);
+    public abstract bool ContainsAnyPoint (IEnumerable<Vector3> worldPoints);
+    public abstract bool ContainsAllPoints (IEnumerable<Vector3> worldPoints);
+
+    public static bool IsInAnyWaterBody (Vector3 worldPoint, out WaterBody outputWB) {
+        foreach(var wb in waterBodies){
+            if(wb.ContainsPoint(worldPoint)){
+                outputWB = wb;
+                return true;
+            }
+        }
+        outputWB = null;
+        return false;
+    }
 
     void Start () {
         if(!IsValid(out var errorMsg)){
@@ -39,77 +54,12 @@ public abstract class WaterBody : MonoBehaviour {
         waterBodies.Add(this);
         AdditionalStartChecks();
         initialized = true;
-
-        bool IsValid (out string message) {
-            if(!ValidateMajorComponents(out message)){
-                return false;
-            }
-            return ValidateTriggers(out message);
-
-            bool ValidateMajorComponents (out string outputMsg) {
-                bool physicsOk = (physics != null);
-                bool fogOk = (fog != null);
-                bool colsOk = CollidersNotNull;
-                if(!physicsOk || !fogOk || !colsOk){
-                    outputMsg = string.Empty;
-                    if(!physicsOk){
-                        outputMsg += $"No {(nameof(WaterPhysics))} assigned on {nameof(WaterBody)} \"{gameObject.name}\"!\n";
-                    }
-                    if(!fogOk){
-                        outputMsg += $"No {nameof(WaterFog)} assigned on {nameof(WaterBody)} \"{gameObject.name}\"!\n";
-                    }
-                    if(!colsOk){
-                        outputMsg += $"One or more NULL {nameof(Collider)}(s) assigned on {nameof(WaterBody)} \"{gameObject.name}\"!\n";
-                    }
-                    return false;
-                }
-                outputMsg = string.Empty;
-                return true;
-            }
-
-            bool ValidateTriggers (out string outputMsg) {
-                outputMsg = string.Empty;
-                foreach(var col in this){
-                    var typeName = col.GetType().ToString();
-                    if(!col.isTrigger){
-                        outputMsg += $"{typeName} isn't a trigger\n";
-                    }
-                    if(col is MeshCollider mc){
-                        if(!mc.convex){
-                            outputMsg += $"{typeName} is not convex\n";
-                        }
-                    }
-                }
-                if(outputMsg.Length > 0){
-                    outputMsg = $"One or more {nameof(Collider)}(s) on {nameof(WaterBody)} \"{gameObject.name}\" have problems:\n{outputMsg}";
-                    return false;
-                }
-                return true;
-            }
-        }
-
-        bool AllTriggersOnWaterLayer (Transform transformToCheck) {
-            var triggerComponent = transformToCheck.GetComponent<Collider>();
-            var transformIsTrigger = triggerComponent != null ? triggerComponent.isTrigger : false;
-            var transformIsOnWaterLayer = transformToCheck.gameObject.layer == Layer.Water.index;
-            if(transformIsTrigger && !transformIsOnWaterLayer){
-                return false;
-            }
-            int cc = transformToCheck.childCount;
-            var childrenOnWaterLayer = true;
-            for(int i=0; i<cc; i++){
-                childrenOnWaterLayer &= AllTriggersOnWaterLayer(transformToCheck.GetChild(i));
-            }
-            return childrenOnWaterLayer;
-        }
     }
 
     protected virtual void AdditionalStartChecks () { }
 
     void OnDestroy () {
-        if(waterBodies.Contains(this)){
-            waterBodies.Remove(this);
-        }
+        waterBodies.Remove(this);
     }
 
     void FixedUpdate () {
@@ -117,25 +67,9 @@ public abstract class WaterBody : MonoBehaviour {
             return;
         }
         torqueNoise = Random.insideUnitSphere;
+        rbs.RemoveNullEntries();
         DoDrag();
         DoBuoyancy();
-        rbs.Clear();
-
-        void DoBuoyancy () {
-            if(Physics.gravity.sqrMagnitude > 0){
-                float densityNormalizer = physics.GetApproxDensityNormalizer();
-                Vector3 gravityDir = Physics.gravity.normalized;
-                if(physics.UseSimpleBuoyancy){
-                    foreach(var rb in rbs){
-                        AddSimpleBuoyancy(rb, gravityDir, densityNormalizer);
-                    }
-                }else{
-                    foreach(var rb in rbs){
-                        AddBetterBuoyancy(rb, gravityDir, densityNormalizer);
-                    }
-                }
-            }
-        }
 
         void DoDrag () {
             Vector3 ownVelocity = Vector3.zero;
@@ -147,9 +81,19 @@ public abstract class WaterBody : MonoBehaviour {
                 AddDrag(rb, ownVelocity);
             }
         }
+
+        void DoBuoyancy () {
+            if(Physics.gravity.sqrMagnitude > 0){
+                float densityNormalizer = waterPhysics.GetApproxDensityNormalizer();
+                Vector3 gravityDir = Physics.gravity.normalized;
+                foreach(var rb in rbs){
+                    AddSimpleBuoyancy(rb, gravityDir, densityNormalizer);
+                }
+            }
+        }
     }
 
-    void OnTriggerStay (Collider otherCollider) {
+    void OnTriggerEnter (Collider otherCollider) {
         if(!initialized){
             return;
         }
@@ -158,6 +102,16 @@ public abstract class WaterBody : MonoBehaviour {
             if(!rbs.Contains(otherRB)){
                 rbs.Add(otherRB);
             }
+        }
+    }
+
+    void OnTriggerExit (Collider otherCollider) {
+        if(!initialized){
+            return;
+        }
+        var otherRB = otherCollider.attachedRigidbody;
+        if(otherRB != null){
+            rbs.Remove(otherRB);
         }
     }
 
@@ -172,11 +126,8 @@ public abstract class WaterBody : MonoBehaviour {
             force: -Physics.gravity * buoyancy,
             mode: ForceMode.Acceleration
         );
-        var torque = GetBuoyancyTorque(rb, gravityDir);
-        // TODO look at this at a later time
-        // unity automatically limits the maximum angular velocity, so i don't have to worry about that
-        // but how to i treat massive objects? 
-        rb.AddTorque(torque * 1000f / rb.mass, ForceMode.Acceleration);
+        var torque = 10f * GetBuoyancyTorque(rb, gravityDir);
+        rb.AddTorque(torque, ForceMode.Acceleration);
     }
 
     public Vector3 GetBuoyancyTorque (Rigidbody rb, Vector3 gravityDir) {
@@ -197,61 +148,9 @@ public abstract class WaterBody : MonoBehaviour {
         }
     }
 
-    void AddBetterBuoyancy (Rigidbody rb, Vector3 gravityDir, float densityNormalizer) {
-        float rbDensity = densityNormalizer * physics.FastApproxDensity(rb);
-        float buoyancy = physics.BuoyancyFromDensity(rbDensity);
-        if(buoyancy == 0f){
-            return;
-        }
-        var rbPos = rb.position;
-        bool applyBuoyancy = false;
-        float depth = 0f;
-        // i need dedicated surface colliders to figure out depth easily
-        // not even colliders... point, normal and 2d-size are enough...
-        // just have to figure out the math
-
-        var boundsTestOffset = gravityDir * MAX_RIGIDBODY_SIZE_ASSUMPTION;
-        var rbTop = rb.ClosestPointOnBounds(rbPos - boundsTestOffset);      // i could just use this to determine the size?
-        var rbBottom = rb.ClosestPointOnBounds(rbPos + boundsTestOffset);   // and use the average as the check position?
-        // find depth of top, bottom and position
-        // use that for the lerping
-        // TODO additional field(s) in physics to determine how high an object can float depending on its drag
-        // TODO import beach ball and texture (no center of mass fudgery!)
-        // TODO also test a log (should just about float, and flat!)
-        // TODO plank (should float well and flat)
-        // TODO other things
-        foreach(var col in this){
-            var topInside = ColliderContainsPoint(col, rbTop);
-            var bottomInside = ColliderContainsPoint(col, rbBottom);
-            if(DepthCast(col, rbPos, gravityDir, out depth)){
-                applyBuoyancy = true;
-                break;  // break here is wrong, this is just legacy code
-            }
-        }
-        if(!applyBuoyancy){
-            return;
-        }
-        // TODO instead of the set neutralization depth value, use the size of the rigidbody (closestpointonbounds?)
-        // to determine the start of the lerp towards 1 and if above the surface, towards 0!
-        // this could also allow the beach ball to float higher than 0
-        // if i make the 1-depth dependent on the drag...
-
-        // if top and bottom are in the water, add at center of mass
-        // otherwise, lerp to position ? or is this too much complexity?
-        
-        if(buoyancy > 1){
-            // buoyancy = Mathf.Lerp(1f, buoyancy, depth / physics.BuoyancyNeutralizationDepth);
-        }
-
-        // zero lerp test point -> closest to center of water body.. i guess active trigger will have to do?
-        // lerp to one only if greater
-        // lerp to zero always
-        AddBuoyancy(rb, buoyancy, gravityDir);
-    }
-
     void AddSimpleBuoyancy (Rigidbody rb, Vector3 gravityDir, float densityNormalizer) {
-        float rbDensity = densityNormalizer * physics.FastApproxDensity(rb);
-        float buoyancy = physics.BuoyancyFromDensity(rbDensity);
+        float rbDensity = densityNormalizer * waterPhysics.FastApproxDensity(rb);
+        float buoyancy = waterPhysics.BuoyancyFromDensity(rbDensity);
         if(buoyancy == 0f){
             return;
         }
@@ -273,12 +172,12 @@ public abstract class WaterBody : MonoBehaviour {
         }
         if(maxInDepth > 0){
             if(buoyancy > 1){
-                buoyancy = Mathf.Lerp(1, buoyancy, maxInDepth / physics.LowerBuoyancyNeutralizationRange);
+                buoyancy = Mathf.Lerp(1, buoyancy, maxInDepth / waterPhysics.LowerBuoyancyNeutralizationRange);
             }
         }else{
             buoyancy = Mathf.Min(buoyancy, 1f);
             if(maxOutDepth > 0){
-                buoyancy *= (1f - Mathf.Clamp01(maxOutDepth / physics.UpperBuoyancyNeutralizationRange));
+                buoyancy *= (1f - Mathf.Clamp01(maxOutDepth / waterPhysics.UpperBuoyancyNeutralizationRange));
             }
         }
         AddBuoyancy(rb, buoyancy, gravityDir);
@@ -299,23 +198,67 @@ public abstract class WaterBody : MonoBehaviour {
         return false;
     }
 
-    public static bool ColliderContainsPoint (Collider col, Vector3 worldPoint) {
-        return (col.ClosestPoint(worldPoint) - worldPoint).sqrMagnitude <= CONTAINS_DIST;
+    bool IsValid (out string message) {
+        if(!ValidateMajorComponents(out message)){
+            return false;
+        }
+        return ValidateTriggers(out message);
+
+        bool ValidateMajorComponents (out string outputMsg) {
+            bool physicsOk = (waterPhysics != null);
+            bool fogOk = (fog != null);
+            bool colsOk = CollidersNotNull;
+            if(!physicsOk || !fogOk || !colsOk){
+                outputMsg = string.Empty;
+                if(!physicsOk){
+                    outputMsg += $"No {(nameof(WaterPhysics))} assigned on {nameof(WaterBody)} \"{gameObject.name}\"!\n";
+                }
+                if(!fogOk){
+                    outputMsg += $"No {nameof(WaterFog)} assigned on {nameof(WaterBody)} \"{gameObject.name}\"!\n";
+                }
+                if(!colsOk){
+                    outputMsg += $"One or more NULL {nameof(Collider)}(s) assigned on {nameof(WaterBody)} \"{gameObject.name}\"!\n";
+                }
+                return false;
+            }
+            outputMsg = string.Empty;
+            return true;
+        }
+
+        bool ValidateTriggers (out string outputMsg) {
+            outputMsg = string.Empty;
+            foreach(var col in this){
+                var typeName = col.GetType().ToString();
+                if(!col.isTrigger){
+                    outputMsg += $"{typeName} isn't a trigger\n";
+                }
+                if(col is MeshCollider mc){
+                    if(!mc.convex){
+                        outputMsg += $"{typeName} is not convex\n";
+                    }
+                }
+            }
+            if(outputMsg.Length > 0){
+                outputMsg = $"One or more {nameof(Collider)}(s) on {nameof(WaterBody)} \"{gameObject.name}\" have problems:\n{outputMsg}";
+                return false;
+            }
+            return true;
+        }
     }
 
-    public abstract bool ContainsPoint (Vector3 worldPoint);
-    public abstract bool ContainsAnyPoint (IEnumerable<Vector3> worldPoints);
-    public abstract bool ContainsAllPoints (IEnumerable<Vector3> worldPoints);
-
-    public static bool IsInAnyWaterBody (Vector3 worldPoint, out WaterBody outputWB) {
-        foreach(var wb in waterBodies){
-            if(wb.ContainsPoint(worldPoint)){
-                outputWB = wb;
-                return true;
-            }
+    bool AllTriggersOnWaterLayer (Transform transformToCheck) {
+        var triggerComponent = transformToCheck.GetComponent<Collider>();
+        var transformIsTrigger = triggerComponent != null ? triggerComponent.isTrigger : false;
+        var transformIsOnWaterLayer = transformToCheck.gameObject.layer == Layer.Water.index;
+        if(transformIsTrigger && !transformIsOnWaterLayer){
+            return false;
         }
-        outputWB = null;
-        return false;
+        int cc = transformToCheck.childCount;
+        var childrenOnWaterLayer = true;
+        for(int i=0; i<cc; i++){
+            childrenOnWaterLayer &= AllTriggersOnWaterLayer(transformToCheck.GetChild(i));
+        }
+        return childrenOnWaterLayer;
     }
 	
 }
