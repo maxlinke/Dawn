8230879fdd9f243fcfadd7b78e269cd63a8df6f8
Max Linke <maxlinke@users.noreply.github.com>
2020-10-15 21:47:43 +0200

    cc testing
    > since collisionflags isn't actually a flags enum, i made an extension method for a proper tostring()

---
+++ Assets/Scripts/Extensions/CollisionFlagsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CollisionFlagsExtensions {
+
+    public static string ToStringImproved (this CollisionFlags flags) {
+        if(flags == CollisionFlags.None){
+            return CollisionFlags.None.ToString();
+        }
+        string output = string.Empty;
+        DoComparison(CollisionFlags.Above);
+        DoComparison(CollisionFlags.Sides);
+        DoComparison(CollisionFlags.Below);
+        if(output.Length > 0){
+            output = output.Remove(output.Length-2);
+        }
+        return output;
+
+        void DoComparison (CollisionFlags compare) {
+            if((flags & compare) != 0){
+                output += $"{compare}, ";
+            }
+        }
+    }
+	
+}
---
+++ Assets/Scripts/Testing/CCTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CCTest : MonoBehaviour {
+
+    [SerializeField] CharacterController cc = default;
+    [SerializeField] KeyCode moveKey = default;
+    [SerializeField] Vector3 moveDelta = default;
+
+    [SerializeField] Transform other;
+    [SerializeField] Rigidbody otherRB;
+    [SerializeField] Vector3 otherSpeed;
+
+    int collisions = 0;
+
+    void Update () {
+        // if(Input.GetKeyDown(moveKey)){
+        //     collisions = 0;
+        //     var flags = cc.Move(moveDelta);
+        //     Debug.Log($"{collisions} hits\n{flags.ToStringImproved()}");
+        // }
+        if(Input.GetKey(moveKey)){
+            if(other != null){
+                other.transform.position += otherSpeed * Time.deltaTime;
+            }
+            cc.Move(moveDelta * Time.deltaTime);
+        }
+    }
+
+    void FixedUpdate () {
+        if(Input.GetKey(moveKey)){
+            if(otherRB != null){
+                otherRB.MovePosition(otherRB.position + (otherSpeed * Time.deltaTime));
+            }
+        }
+    }
+
+    void OnControllerColliderHit (ControllerColliderHit hit) {
+        // collisions++;
+        // Debug.DrawRay(hit.point, hit.normal, Color.magenta, 10f, false);
+    }
+	
+}
