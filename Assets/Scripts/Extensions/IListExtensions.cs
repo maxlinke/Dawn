4cce0af851b59866a0fc0816ac62ac48e38a5706
Max Linke <maxlinke@users.noreply.github.com>
2021-06-15 21:36:14 +0200

    adjusting some list and ilistextensions

---
+++ Assets/Scripts/Extensions/IListExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public static class IListExtensions {
+
+    public static T GetFirst<T> (this IList<T> list) {
+        return list[0];
+    }
+
+    public static T GetLast<T> (this IList<T> list) {
+        return list[list.Count - 1];
+    }
+
+    public static T GetWrapped<T> (this IList<T> list, int index) {
+        if(index < 0){
+            index = list.Count - (-index % list.Count);
+        }
+        if(index >= list.Count){
+            index = index % list.Count;
+        }
+        return list[index];
+    }
+
+    public static string ToStringPerElement<T> (this IList<T> list) {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        for(int i=0; i<list.Count; i++){
+            T t = list[i];
+            if(t == null){
+                sb.AppendLine($"{i}: null");
+            }else{
+                sb.AppendLine($"{i}: {t.ToString()}");
+            }
+        }
+        return $"[{sb.ToString().Trim()}]";
+    }
+	
+}
--- Assets/Scripts/Extensions/ListExtensions.cs
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -9,19 +9,6 @@ public static class ListExtensions {
                 i--;
             }
         }
-    }
-
-    public static string ContentsToString<T> (this List<T> list) {
-        string output = string.Empty;
-        for(int i=0; i<list.Count; i++){
-            T t = list[i];
-            if(t == null){
-                output += $"{i} null\n";
-            }else{
-                output += $"{i} : {t.ToString()}\n";
-            }
-        }
-        return output;
-    }
+    }    
 	
 }
